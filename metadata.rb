3ddc12fce665f26923bca1cfacd2fb07f38ec55b
Thorsten Fischer <thorsten@froschi.org>
2012-06-08 00:09:49 +0200

    Amended for use on lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libpcre", ">= 0.0.1"
+depends          "libselinux", ">= 0.0.1"
+depends          "libffi", ">= 0.0.1"
+depends          "libelf", ">= 0.0.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,23 @@
-packages = %w/
-  libglib2.0-0
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid"
+  include_recipe "libpcre"
+  include_recipe "libselinux"
+
+  packages |= %w/
+    libglib2.0-0
+  /
+when "precise"
+  include_recipe "libpcre"
+  include_recipe "libselinux"
+  include_recipe "libelf"
+  include_recipe "libffi"
+
+  packages |= %w/
+    libglib2.0-0
+  /
+end
 
 packages.each do |pkg|
   package pkg do
